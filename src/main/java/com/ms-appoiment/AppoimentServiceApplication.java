5dc0e73e6fb3bf5783612ac26a6f7df0e28f0efa
Oscar Junca <oscardavidjunca000@gmail.com>
2026-07-21 09:08:04 -0500

    First commit for appoiment service :)

---
+++ src/main/java/com/ms-appoiment/AppoimentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ms_appoiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppoimentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppoimentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ms_appoiment/AppoimentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms_appoiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppoimentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
