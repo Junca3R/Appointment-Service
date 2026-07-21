fcd794f23f2df27f67b2fb0c5dba8e5b777da9cd
Oscar Junca <oscardavidjunca000@gmail.com>
2026-07-21 11:00:13 -0500

    Add AppointmentController with basic REST setup

---
+++ src/main/java/com/appoiment/controller/AppointmentController.java
@@ -0,0 +1,17 @@
+package com.appoiment.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/appintments")
+public class AppointmentController {
+
+  
+    
+    
+}
